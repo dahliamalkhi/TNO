995fa0655bb6f50f09b762adabd75b13bace367d
bosconi <bosconi@users.noreply.github.com>
2014-07-24 21:31:27 +0000

    Configure environment variables on per-host basis, to support concurrent testing in multiple environments.

---
+++ scripts/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+(set -o igncr) 2>/dev/null && set -o igncr; # this comment is required. this should be 2nd line of script.
+# Above line makes this script ignore Windows line endings, to avoid this error without having to run dos2unix: 
+#    $'\r': command not found
+# As per http://stackoverflow.com/questions/14598753/running-bash-script-in-cygwin-on-windows-7
+
+ANT_BIN_PATH=`pwd`/dependencies/apache-ant-1.8.2/bin
+if [[ "${PATH}" != "*apache-ant-1.8.2*" ]]; then
+  echo "  ** Adding ant bin dir to PATH **"
+  export PATH=${ANT_BIN_PATH}:${PATH}
+fi
+
+export JAVA_HOME="C:\Program Files\Java\jdk1.6.0_45"
+
+pushd solr-4.6.1-tno/
+ant ivy-bootstrap
+cd solr
+ant dist
+popd
---
+++ scripts/env.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+(set -o igncr) 2>/dev/null && set -o igncr; # this comment is required. this should be 2nd line of script.
+# Above line makes this script ignore Windows line endings, to avoid this error without having to run dos2unix: 
+#    $'\r': command not found
+# As per http://stackoverflow.com/questions/14598753/running-bash-script-in-cygwin-on-windows-7
+
+# Configurable properties
+export JAVA_HOME="C:\Program Files\Java\jdk1.6.0_45"
+
+##################################################################################
+# Should not need to edit anything below here once stable
+
+JAVA_BIN_PATH=${JAVA_HOME}/jre/bin
+if [ ! -e "${JAVA_BIN_PATH}/java.exe" ]; then
+  echo Cannot find java.exe at ${JAVA_BIN_PATH}. Check Java is installed there ...
+  exit 1
+fi
+
+export PATH=${JAVA_BIN_PATH}:${PATH}
+
+HOST=`hostname`
+if [[ "${HOST}" == "TNO-Scale-Test1" ]]; then
+  TNO_POST_HOSTNAME=localhost
+elif [[ "${HOST}" == "TNO-Scale-Test2" ]]; then
+  TNO_POST_HOSTNAME=TNO-Scale-Test1
+else
+  TNO_POST_HOSTNAME=localhost
+fi
